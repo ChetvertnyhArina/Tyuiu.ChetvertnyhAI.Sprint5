fbb61ac43147c9bda86440ac352ee02ea4e4f08b
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-11-22 23:07:00 +0500

    Task0

--- Tyuiu.ChetvertnyhAI.Sprint5.Task0.V5.Lib/DataService.cs
+++ Tyuiu.ChetvertnyhAI.Sprint5.Task0.V5.Lib/DataService.cs
@@ -6,7 +6,8 @@ namespace Tyuiu.ChetvertnyhAI.Sprint5.Task0.V5.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutputFileTask0.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+
             double y = Math.Log(((double)x + 1) / ((double)x + 2));
             y = Math.Round(y, 3);
             File.WriteAllText(path, Convert.ToString(y));
