20f10a5e275a27b08546aa2fe2419c154d74c50a
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-11-24 01:10:18 +0500

    Task2

--- Tyuiu.ChetvertnyhAI.Sprint5.Task2.V12.Lib/DataService.cs
+++ Tyuiu.ChetvertnyhAI.Sprint5.Task2.V12.Lib/DataService.cs
@@ -2,7 +2,7 @@
 using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.ChetvertnyhAI.Sprint5.Task2.V12.Lib
 {
-    public class DataServicec : ISprint5Task2V12
+    public class DataService : ISprint5Task2V12
     {
         public string SaveToFileTextData(int[,] matrix)
         {
@@ -15,7 +15,56 @@ namespace Tyuiu.ChetvertnyhAI.Sprint5.Task2.V12.Lib
             {
                 File.Delete(path);
             }
+            
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
 
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        matrix[i, j] = 1;
+                    }
+
+                    else
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+
+            string str = "";
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0;j < columns; j++)
+                {
+                    if (j != columns - 1)
+                    {
+                        str = str + matrix[i, j]+";";
+                    }
+
+                    else
+                    {
+                        str = str + matrix[i, j];
+                    }
+                } 
+                if ( i != rows - 1 )
+                {
+                    File.AppendAllText(path, str + Environment.NewLine);
+                }
+
+                else
+                {
+                    File.AppendAllText(path, str);
+                }
+
+                str = "";
+            }
+            return path;
         }
     }
 }
--- Tyuiu.ChetvertnyhAI.Sprint5.Task2.V12/Program.cs
+++ Tyuiu.ChetvertnyhAI.Sprint5.Task2.V12/Program.cs
@@ -3,6 +3,50 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Title = "Спринт #5 | Выполнила: Четвертных А. И. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #12                                                             *");
+        Console.WriteLine("* Выполнила: Четвертных Арина Игоревна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+        Console.WriteLine("* значениями с клавиатуры. Заменить положительные элементы массива на 1,  *");
+        Console.WriteLine("* отрицательные на 0. Результат сохранить в файл OutPutFileTask2.csv и    *");
+        Console.WriteLine("* вывести на консоль.                                                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] mtrx = new int[3, 3] { { -5, -5, 9 },
+                                       { -7, 3, -4 },
+                                       { 8, 7, 9 } };
+
+        int rows = mtrx.GetUpperBound(0) + 1;
+        int columns = mtrx.Length / rows;
+        DataService ds = new DataService();
+
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = ds.SaveToFileTextData(mtrx);
+
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
     }
 }
