18a90a08707790d676cfb1a02c9380f83089c7ad
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-11-25 23:26:17 +0500

    Task6

---
+++ Tyuiu.ChetvertnyhAI.Sprint5.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint5.Task6.V14.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint5.Task6.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\ТИУ\DataSprint5\InPutDataFileTask6V14.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+
+
+
+
+        [TestMethod]
+        public void CheckExistsFile()
+        {
+            string path = @"C:\ТИУ\DataSprint5\InPutDataFileTask6V14.txt";
+            FileInfo FileInfo = new FileInfo(path);
+            bool FileExist = File.Exists(path);
+            Assert.AreEqual(true, FileExist);
+        }
+    }
+
+
+
+
+
+}
---
+++ Tyuiu.ChetvertnyhAI.Sprint5.Task6.V14/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint5.Task6.V14.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #5 | Выполнила: Четвертных А. И. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнила: Четвертных Арина Игоревна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Найти количество знаков препинания в заданной строке.                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+
+        DataService ds = new DataService();
+        string path = @"C:\ТИУ\DataSprint5\InPutDataFileTask6V14.txt";
+
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+
+        Console.WriteLine(res);
+
+        Console.ReadKey();
+    }
+}
