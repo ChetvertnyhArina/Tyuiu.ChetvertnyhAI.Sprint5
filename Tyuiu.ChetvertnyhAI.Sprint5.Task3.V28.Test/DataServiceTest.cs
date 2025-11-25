3479097c8d973657379f35871452188b7c971bbf
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-11-25 18:24:21 +0500

    Task3

---
+++ Tyuiu.ChetvertnyhAI.Sprint5.Task3.V28.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint5.Task3.V28.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint5.Task3.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            FileInfo FileInfo = new FileInfo(path);
+            bool FileExist = File.Exists(path);
+            Assert.AreEqual(true, FileExist);
+        }
+    }
+}
--- Tyuiu.ChetvertnyhAI.Sprint5.Task3.V28/Program.cs
+++ Tyuiu.ChetvertnyhAI.Sprint5.Task3.V28/Program.cs
@@ -1,4 +1,4 @@
-﻿using Tyuiu.ChetvertnyhAI.Sprint5.Task2.V12.Lib;
+﻿using Tyuiu.ChetvertnyhAI.Sprint5.Task3.V28.Lib;
 internal class Program
 {
     private static void Main(string[] args)
@@ -6,44 +6,30 @@ internal class Program
         Console.Title = "Спринт #5 | Выполнила: Четвертных А. И. | ИСТНб-25-1";
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* Спринт #5                                                               *");
-        Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
-        Console.WriteLine("* Задание #2                                                              *");
-        Console.WriteLine("* Вариант #12                                                             *");
+        Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #28                                                             *");
         Console.WriteLine("* Выполнила: Четвертных Арина Игоревна | ИСТНб-25-1                       *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
-        Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
-        Console.WriteLine("* значениями с клавиатуры. Заменить положительные элементы массива на 1,  *");
-        Console.WriteLine("* отрицательные на 0. Результат сохранить в файл OutPutFileTask2.csv и    *");
-        Console.WriteLine("* вывести на консоль.                                                     *");
+        Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить    *");
+        Console.WriteLine("* в бинарный файл OutPutFileTask3.bin и вывести на консоль. Округлить     *");
+        Console.WriteLine("* до трёх знаков после запятой.                                           *"); 
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
 
-        int[,] mtrx = new int[3, 3] { { -5, -5, 9 },
-                                       { -7, 3, -4 },
-                                       { 8, 7, 9 } };
-
-        int rows = mtrx.GetUpperBound(0) + 1;
-        int columns = mtrx.Length / rows;
+        int x = 3;
         DataService ds = new DataService();
 
-        Console.WriteLine("Массив:");
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                Console.Write($"{mtrx[i, j]} \t");
-            }
-            Console.WriteLine();
-        }
-        
+
+        Console.WriteLine("x = " + x);
 
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
 
-        string res = ds.SaveToFileTextData(mtrx);
+        string res = ds.SaveToFileTextData(x);
 
         Console.WriteLine("Файл: " + res);
         Console.WriteLine("Создан!");
