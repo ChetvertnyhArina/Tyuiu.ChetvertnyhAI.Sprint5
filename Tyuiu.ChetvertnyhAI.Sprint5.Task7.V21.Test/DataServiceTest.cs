a834adcf7b4a75516600d3a427d29538f6b06817
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-11-27 23:05:05 +0500

    Task7

---
+++ Tyuiu.ChetvertnyhAI.Sprint5.Task7.V21.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint5.Task7.V21.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint5.Task7.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask7.txt");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.ChetvertnyhAI.Sprint5.Task7.V21/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint5.Task7.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #5 | Выполнила: Четвертных А. И. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнила: Четвертных Арина Игоревна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Удалить все знаки препинания из файла. Полученный результат сохранить в *");
+        Console.WriteLine("* в файл OutPutDataFileTask7V21.txt.                                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+
+        DataService ds = new DataService();
+        string path = @"C:\ТИУ\DataSprint5\InPutDataFileTask7V21.txt";
+        string pathSaveFile = Path.Combine(Path.GetTempPath(), "OutPutFileTask7.txt");
+
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Находится в файле: ");
+        pathSaveFile = ds.LoadDataAndSave(path);
+        Console.WriteLine(pathSaveFile);
+
+        Console.ReadKey();
+    }
+}
